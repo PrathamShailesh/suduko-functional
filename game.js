14760460c430103243d12a968ff4ca63cf588328
Pratham Shailesh Dsouza <prathamshailesh777@gmail.com>
2023-11-10 14:06:42 +0530

    game

--- game.js
+++ game.js
@@ -2,8 +2,10 @@ count = 0;
 ans = 0;
 lose = true;
 
+//random numbers to generate random suduko question in the gamee 
 const randomNumber = Math.floor(Math.random() * 3) + 1;
 
+//different suduko questions
 var problem3 = [
   "-2-43-95-",
   "---6--4-3",
@@ -76,6 +78,7 @@ var solution = [
   "789641532",
 ];
 
+//assigning the numbers if random number is 2 then delete the contents inside the problem and solution array and append problem2 and solution2 to that array 
 if (randomNumber === 2) {
   problem = problem2.slice();
   solution = solution2.slice();
@@ -84,9 +87,13 @@ if (randomNumber === 2) {
   solution = solution3.slice();
 }
 
+
+//background music
 let bgm = new Audio("bg.mp3");
 bgm.play();
 bgm.loop = true;
+
+
 
 window.onload = function () {
   startgame();
@@ -94,7 +101,10 @@ window.onload = function () {
   localStorage;
 };
 
+
+//function to start the game
 function startgame() {
+  //create 9 div's and append it to the html 
   for (let i = 1; i < 10; i++) {
     const number = document.createElement("div");
     number.innerText = i;
@@ -104,30 +114,41 @@ function startgame() {
     document.querySelector(".numbers").appendChild(number);
   }
 
+
+//create 9x9 div's and append it to the html 
   for (let i = 0; i < 9; i++) {
     for (let j = 0; j < 9; j++) {
       const tile = document.createElement("div");
-      if (problem[i][j] != "-") {
-        tile.innerText = problem[i][j];
-        tile.classList.add("tilecolor");
-      }
-      if ([i] == 2 || [i] == 5) {
-        tile.style.borderBottom = "2px solid black";
-      }
-      if ([j] == 3 || [j] == 6) {
-        tile.style.borderLeft = "2px solid black";
-      }
       tile.id = `${i}-${j}`;
       tile.addEventListener("click", select);
       tile.classList.add("tile");
       document.querySelector(".game").appendChild(tile);
+
+      //if the problem array contains "-" then dont add it  
+      if (problem[i][j] != "-") {
+        tile.innerText = problem[i][j];
+        tile.classList.add("tilecolor");
+      }
+
+      //add dark bottom border to separate 9x9 div's by 3
+      if ([i] == 2 || [i] == 5) {
+        tile.style.borderBottom = "2px solid black";
+      }
+      //add dark right line to separate 9x9 div's by 3
+      if ([j] == 3 || [j] == 6) {
+        tile.style.borderLeft = "2px solid black";
+      }
     }
   }
 }
 
 tog = null;
+
+// audio of click sound
 let btn = new Audio("click.mp3");
 
+
+//make the numbers toggle
 function toggle() {
   if (tog != null) {
     tog.classList.remove("tog");
@@ -139,6 +160,8 @@ function toggle() {
   btn.play();
 }
 
+
+//end the game if the player win or lose
 function checkIfGameFinished() {
   if (ans == 46) {
     localStorage.setItem("gameState", "win");
@@ -149,9 +172,13 @@ function checkIfGameFinished() {
   }
 }
 
+
+//audio for the correct ans and wrong answer
 let correct = new Audio("correct.mp3");
 let wr = new Audio("wrong.mp3");
 
+
+//
 function select() {
   if (tog) {
     if (this.innerText != "") {
@@ -162,27 +189,35 @@ function select() {
   let i = num[0];
   let j = num[1];
 
+
+  //apply the numbers inside the box if the answer is correct and play music for every right and wrong answer 
   if (solution[i][j] == tog.id) {
     checkIfGameFinished();
     this.innerText = tog.id;
     ans += 1;
+    //correct answer audio
     correct.pause();
     correct.currentTime = 0;
     correct.play();
   } else {
+    //wrong answer audio
     wr.pause();
     wr.currentTime = 0;
     wr.play();
 
+    //incrementing the wrong answer
     count += 1;
     localStorage.setItem("wrong", count);
     document.querySelector(".wrong_ans h2").innerText = "wrong tries-" + count;
   }
 }
 
+
+//timer for the game
 function timer() {
   const timer = document.querySelector(".time h2");
-  let timeLeft = 100 * 6;
+  let timeLeft = 100*6;
+  //converting the time to minutes and seconds
   function formatTime(seconds) {
     const minutes = Math.floor(seconds / 60);
     const secondsDisplay = seconds % 60;
@@ -197,6 +232,8 @@ function timer() {
     timer.textContent = `Count Down : ${formatTime(timeLeft)}`;
     localStorage.setItem("time", timeLeft);
 
+
+    //if the time becomes 0 redirect it to result page
     if (timeLeft === 0) {
       clearInterval(timerInterval);
       localStorage.setItem("gameState", "lose");
--- result.js
+++ result.js
@@ -1,43 +1,57 @@
-const nickname=localStorage.getItem('playerName')
+const nickname = localStorage.getItem("playerName");
 
-const randomNumber = Math.floor(Math.random() * 3) + 1
+//generating random numbers from 1-3;
+const randomNumber = Math.floor(Math.random() * 3) + 1;
 
-var win_message2=`Congragulations! ${nickname} you've Solved the Sudoku master and conquered the board!`
-var win_message3=`Congragulations! ${nickname} you've Achieved Sudoku supremacy and cracked the ultimate puzzle!`
-var lose_message2=`You Lost! Back to the drawing board  ${nickname} -you will crack it next time.!`
-var lose_message3=`Bad luck, ${nickname}! The complexity of this puzzle outsmarted you this round.`
 
-var win_message=`Congratulations! ${nickname} You've conquered the Sudoku puzzle! Great job!`;
-var lose_message=`You Lost ! Better Luck Next Time ${nickname}`
-
-if (randomNumber === 2) {
-    win_message = win_message2.slice(); 
-    lose_message=lose_message2.slice()
-  
-}
-else if(randomNumber===3){
-    win_message=win_message3.slice();
-    lose_message=lose_message3.slice()
-
-}
-
-let lost = new Audio("lose.mp3")
-
-window.onload = function() {
-    const gameState = localStorage.getItem('gameState');
-    const wrong = localStorage.getItem('wrong')
-    const time =localStorage.getItem('time')
-    const minutes = Math.floor(time / 60);
-    const seconds = time % 60;
+//lost message phrases
+var lost_messge = [
+  `You Lost! Back to the drawing board  ${nickname} -you will crack it next time.!`,
+  `Bad luck, ${nickname}! The complexity of this puzzle outsmarted you this round.`,
+];
+//win message phrase
+var win = [
+    `Congragulations! ${nickname} you've Solved the Sudoku master and conquered the board!`,
+    `Congragulations! ${nickname} you've Achieved Sudoku supremacy and cracked the ultimate puzzle!`
     
-    if (gameState === 'win' ) {
-        document.querySelector(".win").innerText = win_message;
-        document.querySelector(".try h2").innerText= 'Wrong Tries-'+wrong;
-        document.querySelector(".time h2").innerText = `Time Taken: ${minutes} : ${seconds} `;
-    } else {
-        lost.play()
-        document.querySelector(".win").innerText = lose_message;
-        document.querySelector(".try h2").innerText= 'Wrong Tries-'+wrong;
-        document.querySelector(".time h2").innerText = `Time Taken: ${minutes} : ${seconds} `;
-    }
+]
+
+
+var win_message = [`Congratulations! ${nickname} You've conquered the Sudoku puzzle! Great job!`];
+var lose_message = [`You Lost ! Better Luck Next Time ${nickname}`];
+
+//if the random number is 2 or 3 it will change the initial message
+if (randomNumber === 2) {
+  win_message = win[0].slice();
+  lose_message = lost_messge[0].slice();
+} else if (randomNumber === 3) {
+  win_message = win[1].slice();
+  lose_message = lost_messge[1].slice();
+}
+
+
+//lose audio
+let lost = new Audio("lose.mp3");
+
+window.onload = function () {
+  const gameState = localStorage.getItem("gameState");
+  const wrong = localStorage.getItem("wrong");
+  const time = localStorage.getItem("time");
+  const minutes = Math.floor(time / 60);
+  const seconds = time % 60;
+//if the player wins or loses the score and win phrase should display 
+  if (gameState === "win") {
+    document.querySelector(".win").innerText = win_message;
+    document.querySelector(".try h2").innerText = "Wrong Tries-" + wrong;
+    document.querySelector(
+      ".time h2"
+    ).innerText = `Time Taken: ${minutes} : ${seconds} `;
+  } else {
+    lost.play();
+    document.querySelector(".win").innerText = lose_message;
+    document.querySelector(".try h2").innerText = "Wrong Tries-" + wrong;
+    document.querySelector(
+      ".time h2"
+    ).innerText = `Time Taken: ${minutes} : ${seconds} `;
+  }
 };
